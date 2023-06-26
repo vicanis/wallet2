5171ab8bc9af6a0c3adbb1dc720b175ed3b66b24
viktor <viktor@localhost>
2023-06-27 00:47:03 +0500

    refactor: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Menu from "./components/menu";
 
-function App() {
+export default function App() {
     return (
         <div className="App">
             <Menu />
         </div>
     );
 }
-
-export default App;
