3c78d236c9b0a92373b8e5c9b96c73181c159026
viktor <viktor@localhost>
2023-06-28 00:13:40 +0500

    feat: add currency selector component

---
+++ src/components/currency/icon.tsx
@@ -0,0 +1,33 @@
+import { IconProps } from "@mdi/react/dist/IconProps";
+import { CurrencyType } from "./selector";
+import { mdiCurrencyKzt, mdiCurrencyRub, mdiCurrencyUsd } from "@mdi/js";
+import Icon from "@mdi/react";
+
+export function CurrencyIcon({
+    type,
+    ...props
+}: {
+    type: CurrencyType;
+} & Omit<IconProps, "path">) {
+    let svg: string = "";
+
+    switch (type) {
+        case "RUB":
+            svg = mdiCurrencyRub;
+            break;
+
+        case "KZT":
+            svg = mdiCurrencyKzt;
+            break;
+
+        case "USD":
+            svg = mdiCurrencyUsd;
+            break;
+    }
+
+    if (svg === "") {
+        return <span>{type}</span>;
+    }
+
+    return <Icon size={1} {...props} path={svg} />;
+}
---
+++ src/components/currency/selector.tsx
@@ -0,0 +1,22 @@
+import { CurrencyIcon } from "./icon";
+
+export default function Selector({
+    list,
+    selected,
+}: {
+    list: CurrencyType[];
+    selected: CurrencyType;
+}) {
+    return (
+        <span className="flex gap-1 justify-center">
+            {list.map((type) => (
+                <CurrencyIcon
+                    type={type as CurrencyType}
+                    className={selected !== type ? "opacity-50" : ""}
+                />
+            ))}
+        </span>
+    );
+}
+
+export type CurrencyType = "RUB" | "KZT" | "USD";
