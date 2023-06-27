e07cf02763f87a93b676e524f672a1f42a6f8bd2
viktor <viktor@localhost>
2023-06-28 00:14:14 +0500

    feat: add money amount component

---
+++ src/components/amount.tsx
@@ -0,0 +1,24 @@
+import { CurrencyIcon } from "./currency/icon";
+import { CurrencyType } from "./currency/selector";
+
+export default function Amount({
+    currency,
+    value,
+    iconSize = 1,
+    ...rest
+}: {
+    currency: CurrencyType;
+    value: number;
+    iconSize?: number;
+} & React.HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div className="flex gap-1 items-center" {...rest}>
+            <CurrencyIcon type={currency} size={iconSize} />
+            <span>
+                {value.toLocaleString("ru-RU", {
+                    useGrouping: true,
+                })}
+            </span>
+        </div>
+    );
+}
