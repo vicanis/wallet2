d14e31fc5ac59aa5affb60da2cfc812c10bdf1b2
viktor <viktor@localhost>
2023-06-28 22:52:28 +0500

    feat: add browser router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
+import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Dashboard from "../layouts/dashboard";
 import Menu from "../components/menu";
+import ErrorPage from "./error";
 
 export default function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: (
+                <div>
+                    <Outlet />
+                    <Menu />
+                </div>
+            ),
+            errorElement: <ErrorPage />,
+            children: [
+                {
+                    path: "home",
+                    element: <Dashboard />,
+                },
+                {
+                    path: "expense",
+                    element: <span>Expenses</span>,
+                },
+                {
+                    path: "income",
+                    element: <span>Incomes</span>,
+                },
+                {
+                    path: "stats",
+                    element: <span>Stats</span>,
+                },
+                {
+                    path: "exchange",
+                    element: <span>Exchange</span>,
+                },
+            ],
+        },
+    ]);
+
     return (
-        <div className="App">
-            <Dashboard />
-            <Menu />
-        </div>
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
     );
 }
