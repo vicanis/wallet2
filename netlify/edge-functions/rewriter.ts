12fa7483f7bb248291d216b6cc6afad0bdaa08b1
viktor <viktor@localhost>
2023-06-30 23:23:53 +0500

    fix: add request logging to the rewriter edge function

--- netlify/edge-functions/rewriter.ts
+++ netlify/edge-functions/rewriter.ts
@@ -1,6 +1,16 @@
 import type { Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
-    console.log("redirect URL", request.url);
-    return new URL("/", request.url);
+    const url = new URL(request.url);
+
+    console.log(
+        "handle request from URL",
+        request.url,
+        "host",
+        url.hostname,
+        "path",
+        url.pathname
+    );
+
+    // return new URL("/", request.url);
 };
