2f24bd58751502d8ad9c76b08aea175604950ca9
viktor <viktor@localhost>
2023-06-30 23:15:35 +0500

    feat: add netlify edge function to rewrite URLs

--- netlify/edge-functions/rewriter.ts
+++ netlify/edge-functions/rewriter.ts
@@ -1,1 +1,6 @@
-export default () => new Response("Hello world");
+import type { Context } from "@netlify/edge-functions";
+
+export default async (request: Request, context: Context) => {
+    console.log("redirect URL", request.url);
+    return new URL("/", request.url);
+};
