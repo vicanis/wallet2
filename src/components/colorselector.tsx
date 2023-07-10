e50fad5ba497d48d9378cbb2700e7a7f9392b73d
viktor <viktor@localhost>
2023-07-10 09:18:17 +0500

    feat: add color selector component

---
+++ src/components/colorselector.tsx
@@ -0,0 +1,38 @@
+import { mdiPlusCircleOutline } from "@mdi/js";
+import Icon from "@mdi/react";
+
+export default function ColorSelector({
+    selected,
+    onSelect,
+}: {
+    selected: string;
+    onSelect: (color: string) => void;
+}) {
+    function Color({ color }: { color: string }) {
+        return (
+            <div
+                className={`${
+                    selected === color
+                        ? "w-11 h-11 border-[6px] border-[#1F93CE]"
+                        : "w-9 h-9"
+                }`}
+                style={{
+                    backgroundColor: color,
+                    borderRadius: "50%",
+                }}
+                onClick={() => onSelect(color)}
+            ></div>
+        );
+    }
+
+    return (
+        <div className="flex items-center justify-between gap-2">
+            <Color color="#F52D20" />
+            <Color color="#E5AF23" />
+            <Color color="#63B611" />
+            <Color color="#102BBA" />
+            <Color color="#761BBDD6" />
+            <Icon path={mdiPlusCircleOutline} className="w-11 h-11" />
+        </div>
+    );
+}
