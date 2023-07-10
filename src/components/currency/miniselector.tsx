8fc98949449e030197989def7c37fa5306d0b892
viktor <viktor@localhost>
2023-07-10 09:19:04 +0500

    feat: add category editor page

---
+++ src/components/currency/miniselector.tsx
@@ -0,0 +1,44 @@
+import { mdiChevronDown } from "@mdi/js";
+import Icon from "@mdi/react";
+import { CurrencyType } from "./selector";
+import { useState } from "react";
+
+export default function CurrencyMiniSelector({
+    currency,
+    onChange,
+}: {
+    currency: CurrencyType;
+    onChange: (currency: CurrencyType) => void;
+}) {
+    const [opened, setOpened] = useState(false);
+
+    return (
+        <div
+            className="relative text-[#161414]"
+            onClick={() => setOpened((flag) => !flag)}
+        >
+            <div>{currency}</div>
+            <div className="absolute -bottom-4 w-full">
+                <Icon
+                    path={mdiChevronDown}
+                    size={1}
+                    className="mx-auto text-[#1F93CE]"
+                />
+                {opened && (
+                    <div className="absolute w-20 right-0 px-4 py-3 border-2 grid gap-4 bg-white max-h-[200px] overflow-x-hidden overflow-y-auto">
+                        {CurrencyList.map((currency) => (
+                            <div
+                                key={currency}
+                                onClick={() => onChange(currency)}
+                            >
+                                {currency}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+const CurrencyList: CurrencyType[] = ["RUB", "KZT", "USD"];
--- src/pages/settings/category/id.tsx
+++ src/pages/settings/category/id.tsx
@@ -1,36 +1,161 @@
-import React from "react";
-import {
-    Await,
-    LoaderFunctionArgs,
-    defer,
-    useLoaderData,
-} from "react-router-dom";
-import { WithId } from "mongodb";
+import { LoaderFunctionArgs, defer } from "react-router-dom";
+import LoadablePage from "../../../components/loadable";
+import ImageArrowDown from "../../../assets/arrow_down2.svg";
+import ImageArrowUp from "../../../assets/arrow_up2.svg";
+import Tabs from "../../../components/tabs";
+import { ReactNode, useEffect, useReducer, useState } from "react";
+import Icon from "@mdi/react";
+import { mdiListBoxOutline } from "@mdi/js";
+import CurrencyMiniSelector from "../../../components/currency/miniselector";
+import { CurrencyType } from "../../../components/currency/selector";
 import { Category } from "../../../types/category";
-import Blur from "../../../components/blur";
-import LoadingLayout from "../../../layouts/loading";
+import PrimaryButton from "../../../components/button/primary";
+import ColorSelector from "../../../components/colorselector";
 
 export default function CategorySettingsItemPage() {
-    const { item } = useLoaderData() as { item: WithId<Category> };
+    const [activeTab, setActiveTab] = useState("expense");
+
+    const [categoryData, dispatchCategoryData] = useReducer(
+        (state: Category, action: CategoryAction) => {
+            switch (action.type) {
+                case "name":
+                    state.name = action.value;
+                    break;
+
+                case "color":
+                    state.color = action.value;
+                    break;
+
+                case "plan":
+                    state.plan.value = action.value;
+                    break;
+
+                case "currency":
+                    state.plan.currency = action.currency;
+                    break;
+            }
+
+            return { ...state };
+        },
+        {
+            name: "",
+            plan: {
+                currency: "RUB",
+            },
+            color: "#F52D20",
+        }
+    );
+
+    useEffect(() => {
+        console.log("category data updated", categoryData);
+    }, [categoryData]);
 
     return (
-        <div>
-            <React.Suspense
-                fallback={
-                    <Blur>
-                        <LoadingLayout>Загрузка данных ...</LoadingLayout>
-                    </Blur>
-                }
-            >
-                <Await resolve={item} errorElement={<div>Error loading</div>}>
-                    {(data) => (
-                        <div>
-                            Category item page, loader data?{" "}
-                            {JSON.stringify(data)}
+        <LoadablePage
+            renderer={(data) => (
+                <div>
+                    <Tabs
+                        tabs={[
+                            {
+                                id: "expense",
+                                icon: ImageArrowUp,
+                                name: "Расход",
+                            },
+                            {
+                                id: "income",
+                                icon: ImageArrowDown,
+                                name: "Доход",
+                            },
+                        ]}
+                        selected="expense"
+                        onSelect={(tab) => {
+                            setActiveTab(tab.id);
+                        }}
+                    />
+
+                    <div className="p-6 text-[#0084C8] grid gap-6">
+                        <div className="flex items-center gap-3">
+                            <Icon path={mdiListBoxOutline} size={1.5} />
+
+                            <input
+                                className="border-b-2 border-b-[#0084C8] w-full placeholder-[#8A8181]"
+                                type="text"
+                                placeholder="Название категории"
+                                defaultValue={categoryData.name}
+                                onChange={(event) =>
+                                    dispatchCategoryData({
+                                        type: "name",
+                                        value: event.target.value,
+                                    })
+                                }
+                            />
                         </div>
-                    )}
-                </Await>
-            </React.Suspense>
+
+                        <Block
+                            title={
+                                activeTab === "expense"
+                                    ? "Планирую тратить"
+                                    : "Планирую получать"
+                            }
+                        >
+                            <div className="flex items-center gap-4">
+                                <input
+                                    type="text"
+                                    placeholder="не задано"
+                                    className="border-b-2 border-b-[#0084C8] placeholder-[#8A8181] max-w-[125px]"
+                                    defaultValue={categoryData.plan.value ?? 0}
+                                    onChange={(event) =>
+                                        dispatchCategoryData({
+                                            type: "plan",
+                                            value: Number(event.target.value),
+                                        })
+                                    }
+                                />
+                                <CurrencyMiniSelector
+                                    currency={
+                                        categoryData.plan.currency ?? "RUB"
+                                    }
+                                    onChange={(currency) =>
+                                        dispatchCategoryData({
+                                            type: "currency",
+                                            currency,
+                                        })
+                                    }
+                                />
+                                <div className="text-[#161414]">в месяц</div>
+                            </div>
+                        </Block>
+
+                        <Block title="Иконки">
+                            <div>Icon selector</div>
+                        </Block>
+
+                        <Block title="Цвет">
+                            <ColorSelector
+                                selected={categoryData.color}
+                                onSelect={(color) =>
+                                    dispatchCategoryData({
+                                        type: "color",
+                                        value: color,
+                                    })
+                                }
+                            />
+                        </Block>
+
+                        <PrimaryButton title="Добавить" />
+                    </div>
+                </div>
+            )}
+        />
+    );
+}
+
+function Block({ title, children }: { title: ReactNode; children: ReactNode }) {
+    return (
+        <div className="grid gap-3">
+            <div className="text-[#8A8181]">{title}</div>
+
+            {children}
         </div>
     );
 }
@@ -44,12 +169,12 @@ export function CategoryItemLoader({ params }: LoaderFunctionArgs) {
 
     if (id === "new") {
         return defer({
-            item: {},
+            data: {},
         });
     }
 
     return defer({
-        item: Loader(id),
+        data: Loader(id),
     });
 }
 
@@ -59,3 +184,17 @@ async function Loader(id: string) {
 
     return data;
 }
+
+type CategoryAction =
+    | {
+          type: "name" | "color";
+          value: string;
+      }
+    | {
+          type: "plan";
+          value: number;
+      }
+    | {
+          type: "currency";
+          currency: CurrencyType;
+      };
