9fcbeaff07b536475ff66d6dd59c50b6c89a842a
viktor <viktor@localhost>
2023-07-10 09:24:28 +0500

    fix: horizontal position of the items after the category color change

--- src/components/colorselector.tsx
+++ src/components/colorselector.tsx
@@ -11,10 +11,8 @@ export default function ColorSelector({
     function Color({ color }: { color: string }) {
         return (
             <div
-                className={`${
-                    selected === color
-                        ? "w-11 h-11 border-[6px] border-[#1F93CE]"
-                        : "w-9 h-9"
+                className={`w-11 h-11 border-[6px] ${
+                    selected === color ? "border-[#1F93CE]" : "border-white"
                 }`}
                 style={{
                     backgroundColor: color,
