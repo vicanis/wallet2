9ececc83ce50ad3186acfd66efb570b5e4a0aa07
viktor <viktor@localhost>
2023-07-11 09:31:37 +0500

    feat: show loading overlay during primary button onClick handling

--- src/components/button/primary.tsx
+++ src/components/button/primary.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import Blur from "../blur";
+import LoadingLayout from "../../layouts/loading";
+
 export default function PrimaryButton({
     title,
     onClick,
 }: {
     title: string;
-    onClick?: () => void;
+    onClick?: () => void | Promise<void>;
 }) {
+    const [busy, setBusy] = useState(false);
+
+    if (busy) {
+        return (
+            <Blur>
+                <LoadingLayout />
+            </Blur>
+        );
+    }
+
     return (
         <div
             className="w-max mx-auto px-8 py-4 rounded-xl font-semibold"
@@ -12,7 +26,19 @@ export default function PrimaryButton({
                 backgroundColor: "#1F93CE",
                 color: "#F6FCFF",
             }}
-            onClick={onClick}
+            onClick={async () => {
+                if (typeof onClick !== "function") {
+                    return;
+                }
+
+                setBusy(true);
+
+                try {
+                    await onClick();
+                } finally {
+                    setBusy(false);
+                }
+            }}
         >
             {title}
         </div>
