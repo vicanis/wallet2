ef8474d1fbba84053cd8f468628f6d292072b76f
viktor <viktor@localhost>
2023-07-11 10:29:10 +0500

    refactor: extract category editor into separate component

--- src/pages/settings/category/id.tsx
+++ src/pages/settings/category/id.tsx
@@ -1,199 +1,9 @@
-import { ReactNode, useLayoutEffect, useReducer } from "react";
-import { LoaderFunctionArgs, defer, useNavigate } from "react-router-dom";
-import Icon from "@mdi/react";
-import { mdiListBoxOutline } from "@mdi/js";
-import { Category } from "../../../types/category";
+import { LoaderFunctionArgs, defer } from "react-router-dom";
 import LoadablePage from "../../../components/loadable";
-import CurrencyMiniSelector from "../../../components/currency/miniselector";
-import { CurrencyType } from "../../../components/currency/selector";
-import PrimaryButton from "../../../components/button/primary";
-import ColorSelector from "../../../components/colorselector";
-import IconSelector from "../../../components/category/icon/selector";
-import CategoryTypeTabs from "../../../layouts/category/typetabs";
+import CategoryEditor from "../../../layouts/category/editor";
 
 export default function CategorySettingsItemPage() {
-    const navigate = useNavigate();
-
-    const [categoryData, dispatchCategoryData] = useReducer(
-        (state: Category, action: CategoryAction) => {
-            switch (action.type) {
-                case "type":
-                    state.type = action.value;
-                    break;
-
-                case "name":
-                    state.name = action.value;
-                    break;
-
-                case "color":
-                    state.color = action.value;
-                    break;
-
-                case "icon":
-                    state.icon = action.value;
-                    break;
-
-                case "plan":
-                    state.plan.value = action.value;
-                    break;
-
-                case "currency":
-                    state.plan.currency = action.currency;
-                    break;
-            }
-
-            return { ...state };
-        },
-        {
-            type: "expense",
-            name: "",
-            plan: {
-                currency: "RUB",
-            },
-            color: "#F52D20",
-        }
-    );
-
-    useLayoutEffect(() => {
-        switch (categoryData.type) {
-            case "expense":
-                dispatchCategoryData({
-                    type: "icon",
-                    value: "grocery",
-                });
-                break;
-
-            case "income":
-                dispatchCategoryData({
-                    type: "icon",
-                    value: "card",
-                });
-                break;
-        }
-    }, [categoryData.type]);
-
-    return (
-        <LoadablePage
-            renderer={(data) => (
-                <div>
-                    <CategoryTypeTabs
-                        selected={categoryData.type}
-                        onSelect={(tab) => {
-                            dispatchCategoryData({
-                                type: "type",
-                                value: tab,
-                            });
-                        }}
-                    />
-
-                    <div className="p-6 text-[#0084C8] grid gap-6">
-                        <div className="flex items-center gap-3">
-                            <Icon path={mdiListBoxOutline} size={1.5} />
-
-                            <input
-                                className="border-b-2 border-b-[#0084C8] w-full placeholder-[#8A8181]"
-                                type="text"
-                                placeholder="Название категории"
-                                defaultValue={categoryData.name}
-                                onChange={(event) =>
-                                    dispatchCategoryData({
-                                        type: "name",
-                                        value: event.target.value,
-                                    })
-                                }
-                            />
-                        </div>
-
-                        <Block
-                            title={
-                                categoryData.type === "expense"
-                                    ? "Планирую тратить"
-                                    : "Планирую получать"
-                            }
-                        >
-                            <div className="flex items-center gap-4">
-                                <input
-                                    type="text"
-                                    placeholder="не задано"
-                                    className="border-b-2 border-b-[#0084C8] placeholder-[#8A8181] max-w-[125px]"
-                                    defaultValue={categoryData.plan.value ?? 0}
-                                    onChange={(event) =>
-                                        dispatchCategoryData({
-                                            type: "plan",
-                                            value: Number(event.target.value),
-                                        })
-                                    }
-                                />
-                                <CurrencyMiniSelector
-                                    currency={
-                                        categoryData.plan.currency ?? "RUB"
-                                    }
-                                    onChange={(currency) =>
-                                        dispatchCategoryData({
-                                            type: "currency",
-                                            currency,
-                                        })
-                                    }
-                                />
-                                <div className="text-[#161414]">в месяц</div>
-                            </div>
-                        </Block>
-
-                        <Block title="Иконки">
-                            <IconSelector
-                                type={categoryData.type}
-                                color={categoryData.color ?? ""}
-                                selected={categoryData.icon ?? "grocery"}
-                                onSelect={(icon) =>
-                                    dispatchCategoryData({
-                                        type: "icon",
-                                        value: icon,
-                                    })
-                                }
-                            />
-                        </Block>
-
-                        <Block title="Цвет">
-                            <ColorSelector
-                                selected={categoryData.color ?? ""}
-                                onSelect={(color) =>
-                                    dispatchCategoryData({
-                                        type: "color",
-                                        value: color,
-                                    })
-                                }
-                            />
-                        </Block>
-
-                        <PrimaryButton
-                            title="Добавить"
-                            onClick={async () => {
-                                await fetch(
-                                    "/.netlify/functions/add_category",
-                                    {
-                                        method: "POST",
-                                        body: JSON.stringify(categoryData),
-                                    }
-                                );
-
-                                navigate(-1);
-                            }}
-                        />
-                    </div>
-                </div>
-            )}
-        />
-    );
-}
-
-function Block({ title, children }: { title: ReactNode; children: ReactNode }) {
-    return (
-        <div className="grid gap-3">
-            <div className="text-[#8A8181]">{title}</div>
-
-            {children}
-        </div>
-    );
+    return <LoadablePage renderer={(data) => <CategoryEditor {...data} />} />;
 }
 
 export function CategoryItemLoader({ params }: LoaderFunctionArgs) {
@@ -205,7 +15,14 @@ export function CategoryItemLoader({ params }: LoaderFunctionArgs) {
 
     if (id === "new") {
         return defer({
-            data: {},
+            data: {
+                type: "expense",
+                name: "",
+                plan: {
+                    currency: "RUB",
+                },
+                color: "#F52D20",
+            },
         });
     }
 
@@ -220,21 +37,3 @@ async function Loader(id: string) {
 
     return data;
 }
-
-type CategoryAction =
-    | {
-          type: "type";
-          value: "expense" | "income";
-      }
-    | {
-          type: "name" | "color" | "icon";
-          value: string;
-      }
-    | {
-          type: "plan";
-          value: number;
-      }
-    | {
-          type: "currency";
-          currency: CurrencyType;
-      };
