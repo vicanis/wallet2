f225f8cc691d0f47e93f901ade1f9b6c6ae5427a
viktor <viktor@localhost>
2023-07-11 23:24:45 +0500

    feat: add "mode" prop to blur overlay component

--- src/components/blur.tsx
+++ src/components/blur.tsx
@@ -2,11 +2,17 @@ import { ReactNode } from "react";
 
 export default function Blur({
     children,
+    mode = "fixed",
     ...props
-}: { children?: ReactNode } & React.HTMLAttributes<HTMLDivElement>) {
+}: {
+    children?: ReactNode;
+    mode?: "fixed" | "absolute";
+} & React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div
-            className="fixed top-0 left-0 w-full h-full backdrop-blur-md z-10"
+            className={`${
+                mode === "fixed" ? "fixed" : "absolute"
+            } top-0 left-0 w-full h-full backdrop-blur-md z-10`}
             {...props}
         >
             {children}
