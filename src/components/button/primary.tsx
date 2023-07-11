da36ea22f79b7db5fe7ef08b8944d7df68132bc7
viktor <viktor@localhost>
2023-07-11 22:02:06 +0500

    feat: add "disabled" prop to primary button component

--- src/components/button/primary.tsx
+++ src/components/button/primary.tsx
@@ -4,9 +4,11 @@ import LoadingLayout from "../../layouts/loading";
 
 export default function PrimaryButton({
     title,
+    disabled = false,
     onClick,
 }: {
     title: string;
+    disabled?: boolean;
     onClick?: () => void | Promise<void>;
 }) {
     const [busy, setBusy] = useState(false);
@@ -21,11 +23,9 @@ export default function PrimaryButton({
 
     return (
         <div
-            className="w-max mx-auto px-8 py-4 rounded-xl font-semibold"
-            style={{
-                backgroundColor: "#1F93CE",
-                color: "#F6FCFF",
-            }}
+            className={`w-max mx-auto px-8 py-4 rounded-xl font-semibold bg-[#1F93CE] text-[#F6FCFF] ${
+                disabled ? "opacity-50" : ""
+            }`}
             onClick={async () => {
                 if (typeof onClick !== "function") {
                     return;
