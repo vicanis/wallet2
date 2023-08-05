e24adf2ce78f70ef046a85b45ca57d99924d4ef5
viktor <viktor@localhost>
2023-08-05 15:38:27 +0400

    chore: add relative time and calendar dayjs plugins

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,6 +1,12 @@
 import dayjs from "dayjs";
-import "dayjs/locale/ru";
 
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+import calendar from "dayjs/plugin/calendar";
+dayjs.extend(calendar);
+
+import "dayjs/locale/ru";
 dayjs.locale("ru");
 
 export default dayjs;
