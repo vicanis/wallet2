9fcb25a8f6040e05058e6f1af63c4e97e2cddd00
viktor <viktor@localhost>
2023-08-09 10:54:13 +0400

    feat: add spinner size prop

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,10 +1,10 @@
 import Icon from "@mdi/react";
 import { mdiLoading } from "@mdi/js";
 
-export default function Spinner() {
+export default function Spinner({ size = 12 }: { size?: number }) {
     return (
-        <div className="animate-spin w-12 h-12 mx-auto">
-            <Icon path={mdiLoading} size={2} />
+        <div className={`animate-spin w-${size} h-${size} mx-auto`}>
+            <Icon path={mdiLoading} size={size / 6} />
         </div>
     );
 }
