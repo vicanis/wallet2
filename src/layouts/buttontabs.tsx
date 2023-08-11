188d89bf6b547cff22c94c91361149b95c3edb5d
viktor <viktor@localhost>
2023-08-11 22:49:44 +0400

    fix: key prop to the button tabs component

--- src/layouts/buttontabs.tsx
+++ src/layouts/buttontabs.tsx
@@ -13,7 +13,11 @@ export default function ButtonTabsLayout({
     return (
         <div className="flex justify-around items-center p-4 text-center bg-[#0084c8] text-white text-sm rounded-b-lg">
             {items.map((item, index) => (
-                <Link to={item.link} className="flex flex-col gap-2">
+                <Link
+                    key={index}
+                    to={item.link}
+                    className="flex flex-col gap-2"
+                >
                     {item.icon}
                     <div>{item.text}</div>
                 </Link>
