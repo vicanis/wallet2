aa4cceb5fac39e71b7aeb39666a24403093d38cb
viktor <viktor@localhost>
2023-08-16 07:52:42 +0500

    style: add grid area layout to the transfer history item

--- src/layouts/transfer/history/item.tsx
+++ src/layouts/transfer/history/item.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import dayjs from "../../../lib/dayjs";
 import Icon from "@mdi/react";
 import { mdiArrowDown } from "@mdi/js";
@@ -5,7 +6,6 @@ import { TransferItem } from "../../../types/transfer";
 import { Wallet } from "../../../types/wallet";
 import Amount from "../../../components/amount";
 import CategoryIcon from "../../../components/category/icon";
-import { Fragment } from "react";
 
 export default function TransferHistoryItem({
     date,
@@ -19,27 +19,46 @@ export default function TransferHistoryItem({
         <div className="grid gap-2">
             <div className="border-b-[1px] px-4">
                 {dayjs(date).format("DD MMMM, dddd")}
+                {typeof user !== "undefined" && (
+                    <span className="text-sm">{user}</span>
+                )}
             </div>
-            <div className="px-4 flex items-center gap-3">
-                <Icon path={mdiArrowDown} size={1} />
-                <div className="grid gap-2 grow">
+            <div
+                className="px-4 gap-3 grid items-center"
+                style={{
+                    gridTemplateAreas: `'arrow walletsrc amountsrc' 'arrow walletdst amountdst'`,
+                    gridTemplateColumns: "max-content 1fr max-content",
+                }}
+            >
+                <div style={{ gridArea: "arrow" }}>
+                    <Icon path={mdiArrowDown} size={1} />
+                </div>
+                <div style={{ gridArea: "walletsrc" }}>
                     <WalletItem {...src} />
+                </div>
+                <div style={{ gridArea: "walletdst" }}>
                     <WalletItem {...dst} />
                 </div>
-                <div className="text-right">
-                    <div className="flex flex-col items-end">
+                {typeof amountDst === "undefined" ? (
+                    <div style={{ gridRow: "1 / 3" }}>
                         <Amount iconSize={0.7} {...amount} />
-                        {typeof amountDst !== "undefined" && (
-                            <Fragment>
-                                <Icon path={mdiArrowDown} size={1} />
-                                <Amount iconSize={0.7} {...amountDst} />
-                            </Fragment>
-                        )}
                     </div>
-                    {typeof user !== "undefined" && (
-                        <span className="text-sm">{user}</span>
-                    )}
-                </div>
+                ) : (
+                    <Fragment>
+                        <div
+                            className="justify-self-end"
+                            style={{ gridArea: "amountsrc" }}
+                        >
+                            <Amount iconSize={0.7} {...amount} />
+                        </div>
+                        <div
+                            className="justify-self-end"
+                            style={{ gridArea: "amountdst" }}
+                        >
+                            <Amount iconSize={0.7} {...amountDst} />
+                        </div>
+                    </Fragment>
+                )}
             </div>
         </div>
     );
