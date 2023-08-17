f12fbf93230c0b638437e76c32e877e8c354a0f9
viktor <viktor@localhost>
2023-08-17 22:28:19 +0500

    style: add currency flags

--- src/components/currency/flag.tsx
+++ src/components/currency/flag.tsx
@@ -1,3 +1,6 @@
+import { ReactNode } from "react";
+import Icon from "@mdi/react";
+import { mdiCurrencyBtc, mdiCurrencyEur, mdiGold, mdiHelp } from "@mdi/js";
 import { CurrencyType } from "../../types/currency";
 
 export default function CurrencyFlag({
@@ -7,20 +10,35 @@ export default function CurrencyFlag({
     currency: CurrencyType;
     className?: string;
 }) {
-    const flags: {
-        [code: string]: string;
-    } = {
-        AED: "ae",
-        KGS: "kg",
-        KZT: "kz",
-        RUB: "ru",
-        USD: "us",
-    };
+    const Container = ({ children }: { children: ReactNode }) => (
+        <div className={className}>{children}</div>
+    );
+
+    const IconItem = ({ icon }: { icon: string }) => (
+        <Container>
+            <Icon path={icon} size={1} />
+        </Container>
+    );
+
+    switch (currency) {
+        case "EUR":
+            return <IconItem icon={mdiCurrencyEur} />;
+
+        case "BTC":
+            return <IconItem icon={mdiCurrencyBtc} />;
+
+        case "XAG":
+        case "XAU":
+            return <IconItem icon={mdiGold} />;
+
+        case "XDR":
+            return <IconItem icon={mdiHelp} />;
+    }
 
     const country = flags[currency] ?? "";
 
     return (
-        <div className={className}>
+        <Container>
             <span
                 className={`fi fi-${country} fis`}
                 style={{
@@ -29,8 +47,185 @@ export default function CurrencyFlag({
                     borderRadius: "50%",
                 }}
             ></span>
-        </div>
+        </Container>
     );
 }
 
-const Flags = {};
+const flags: {
+    [code: string]: string;
+} = {
+    AFN: "af",
+    ALL: "al",
+    DZD: "dz",
+    USD: "vi",
+    AOA: "ao",
+    XCD: "vc",
+    ARS: "ar",
+    AMD: "am",
+    AWG: "aw",
+    AUD: "tv",
+    AZN: "az",
+    BSD: "bs",
+    BHD: "bh",
+    BDT: "bd",
+    BBD: "bb",
+    BOB: "bo",
+    BYN: "by",
+    BYR: "by",
+    BZD: "bz",
+    XOF: "tg",
+    BMD: "bm",
+    INR: "in",
+    BOV: "bo",
+    BAM: "ba",
+    BWP: "bw",
+    NOK: "sj",
+    BRL: "br",
+    BND: "bn",
+    BTN: "bt",
+    BGN: "bg",
+    BIF: "bi",
+    CVE: "cv",
+    KHR: "kh",
+    XAF: "ga",
+    CAD: "ca",
+    KYD: "ky",
+    CLP: "cl",
+    CLF: "cl",
+    COP: "co",
+    CNY: "cn",
+    COU: "co",
+    KMF: "km",
+    CDF: "cd",
+    NZD: "tk",
+    CRC: "cr",
+    HRK: "hr",
+    CUP: "cu",
+    CUC: "cu",
+    ANG: "sx",
+    DKK: "gl",
+    DJF: "dj",
+    DOP: "do",
+    EGP: "eg",
+    ERN: "er",
+    ETB: "et",
+    FKP: "fk",
+    FJD: "fj",
+    XPF: "wf",
+    GMD: "gm",
+    GEL: "ge",
+    GHS: "gh",
+    GIP: "gi",
+    GTQ: "gt",
+    GBP: "gb",
+    GGP: "gg",
+    GNF: "gn",
+    GYD: "gy",
+    CZK: "cz",
+    HNL: "hn",
+    HKD: "hk",
+    HTG: "ht",
+    HUF: "hu",
+    IMP: "im",
+    ISK: "is",
+    IDR: "id",
+    IRR: "ir",
+    IQD: "iq",
+    ILS: "il",
+    JEP: "je",
+    JMD: "jm",
+    JPY: "jp",
+    JOD: "jo",
+    KZT: "kz",
+    KES: "ke",
+    KPW: "kp",
+    KRW: "kr",
+    KWD: "kw",
+    KGS: "kg",
+    LAK: "la",
+    LBP: "lb",
+    ZAR: "za",
+    LRD: "lr",
+    LSL: "ls",
+    LTL: "lt",
+    LVL: "lv",
+    LYD: "ly",
+    CHF: "li",
+    MOP: "mo",
+    MGA: "mg",
+    MWK: "mw",
+    MYR: "my",
+    MVR: "mv",
+    MRU: "mr",
+    MRO: "mr",
+    MUR: "mu",
+    MXV: "mx",
+    MDL: "md",
+    MNT: "mn",
+    MAD: "eh",
+    MXN: "mx",
+    MZN: "mz",
+    MMK: "mm",
+    NAD: "na",
+    NPR: "np",
+    NIO: "ni",
+    NGN: "ng",
+    OMR: "om",
+    PAB: "pa",
+    PKR: "pk",
+    PGK: "pg",
+    PYG: "py",
+    PEN: "pe",
+    PHP: "ph",
+    PLN: "pl",
+    QAR: "qa",
+    MKD: "mk",
+    RON: "ro",
+    RUB: "ru",
+    RWF: "rw",
+    SHP: "sh",
+    WST: "ws",
+    STN: "st",
+    STD: "st",
+    SAR: "sa",
+    RSD: "rs",
+    SCR: "sc",
+    SLE: "sl",
+    SLL: "sl",
+    SGD: "sg",
+    SBD: "sb",
+    SOS: "so",
+    SSP: "ss",
+    LKR: "lk",
+    SDG: "sd",
+    SRD: "sr",
+    SEK: "se",
+    SVC: "sv",
+    SZL: "sz",
+    CHW: "ch",
+    SYP: "sy",
+    TWD: "tw",
+    TJS: "tj",
+    TZS: "tz",
+    THB: "th",
+    TOP: "to",
+    TTD: "tt",
+    TND: "tn",
+    TRY: "tr",
+    TMT: "tm",
+    UGX: "ug",
+    UAH: "ua",
+    AED: "ae",
+    USN: "us",
+    UYU: "uy",
+    UZS: "uz",
+    VUV: "vu",
+    VED: "ve",
+    VEF: "ve",
+    VES: "ve",
+    VND: "vn",
+    YER: "ye",
+    ZMW: "zm",
+    ZMK: "zm",
+    ZWL: "zw",
+};
