8d6e7178b2be4fc4d0cf0c14d743b4e4ab0db3a4
viktor <viktor@localhost>
2023-08-17 11:15:11 +0500

    feat: redirect to login page if auth error during page loading

--- src/components/loadable.tsx
+++ src/components/loadable.tsx
@@ -1,7 +1,13 @@
-import React, { ReactNode } from "react";
-import { Await, useLoaderData } from "react-router-dom";
+import React, { ReactNode, useEffect } from "react";
+import {
+    Await,
+    useAsyncError,
+    useLoaderData,
+    useNavigate,
+} from "react-router-dom";
 import LoadingLayout from "../layouts/loading";
 import Blur from "./blur";
+import { AuthError } from "../lib/auth";
 
 export default function LoadablePage({
     renderer,
@@ -18,12 +24,23 @@ export default function LoadablePage({
                 </Blur>
             }
         >
-            <Await
-                resolve={data}
-                errorElement={<div>Ошибка загрузки данных</div>}
-            >
+            <Await resolve={data} errorElement={<ErrorElement />}>
                 {renderer}
             </Await>
         </React.Suspense>
     );
+}
+
+function ErrorElement() {
+    const navigate = useNavigate();
+
+    const error = useAsyncError() as Error;
+
+    useEffect(() => {
+        if (error instanceof AuthError) {
+            navigate("/login");
+        }
+    }, [error]);
+
+    return <div>Ошибка загрузки данных: {error.message}</div>;
 }
