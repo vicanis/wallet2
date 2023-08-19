82c03fb6746351d43e2bc55d7952167b14a1cc18
viktor <viktor@localhost>
2023-08-20 01:03:46 +0500

    refactor: remove component definition during render to prevent whole subtree re-rendering

--- src/components/colorselector.tsx
+++ src/components/colorselector.tsx
@@ -1,39 +1,14 @@
 import { mdiCheck, mdiPlusCircle } from "@mdi/js";
 import Icon from "@mdi/react";
 
-export default function ColorSelector({
-    selected,
-    onSelect,
-}: {
-    selected: string;
-    onSelect: (color: string) => void;
-}) {
-    function Color({ color }: { color: string }) {
-        return (
-            <div
-                className={`flex items-center justify-center ${
-                    color === selected ? "w-12 h-12" : "w-8 h-8 opacity-50"
-                }`}
-                style={{
-                    backgroundColor: color,
-                    borderRadius: "50%",
-                }}
-                onClick={() => onSelect(color)}
-            >
-                {color === selected && (
-                    <Icon path={mdiCheck} size={1.5} color="black" />
-                )}
-            </div>
-        );
-    }
-
+export default function ColorSelector(props: ColorSelectorProps) {
     return (
         <div className="flex items-center justify-between gap-2">
-            <Color color="#F52D20" />
-            <Color color="#E5AF23" />
-            <Color color="#63B611" />
-            <Color color="#102BBA" />
-            <Color color="#761BBDD6" />
+            <ColorItem color="#F52D20" {...props} />
+            <ColorItem color="#E5AF23" {...props} />
+            <ColorItem color="#63B611" {...props} />
+            <ColorItem color="#102BBA" {...props} />
+            <ColorItem color="#761BBDD6" {...props} />
             <Icon
                 path={mdiPlusCircle}
                 className="w-11 h-11 text-[#0084C8] text-opacity-50"
@@ -41,3 +16,31 @@ export default function ColorSelector({
         </div>
     );
 }
+
+function ColorItem({
+    color,
+    selected,
+    onSelect,
+}: { color: string } & ColorSelectorProps) {
+    return (
+        <div
+            className={`flex items-center justify-center ${
+                color === selected ? "w-12 h-12" : "w-8 h-8 opacity-50"
+            }`}
+            style={{
+                backgroundColor: color,
+                borderRadius: "50%",
+            }}
+            onClick={() => onSelect(color)}
+        >
+            {color === selected && (
+                <Icon path={mdiCheck} size={1.5} color="black" />
+            )}
+        </div>
+    );
+}
+
+type ColorSelectorProps = {
+    selected: string;
+    onSelect: (color: string) => void;
+};
